d4e7656cad68e5ca2bd79cd899e183cdc4cecccd
kapildevsharma02 <kdevsharma012@gmail.com>
2024-07-08 23:49:41 +0530

    This is the change for all the promise function like Promise.all, Promise.allSettled, Promise.race, Promise.any Promise.resolve(), Promise.reject()

--- web_devlopment_practise/PromiseAPIScript.js
+++ web_devlopment_practise/PromiseAPIScript.js
@@ -5,15 +5,16 @@
 let p1=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         // console.log("Value 1")
-        resolve("Value 1")
+        // resolve("Value 1")
+        resolve(4);
     },1000)
 })
 
 let p2=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         // console.log("Value 2")
-        resolve("Value 2")
-        // reject(new Error("This is the error in Promise 2"))
+        // resolve("Value 2")
+        reject(new Error("This is the error in Promise 2"))
     },2000)
 })
 
@@ -41,8 +42,26 @@ let p3=new Promise((resolve,reject)=>{
 //     console.log(value)
 // })
 
-let promise_all=Promise.race([p1,p2,p3])
+let promise_all=Promise.all([p1,p2,p3])
 promise_all.then((value)=>{
     console.log(value);
 })
+
+
+let promise_all_settled=Promise.allSettled([p1,p2,p3])
+promise_all_settled.then((value)=>{
+    console.log(value);
+})
+
+let promise_all_race=Promise.race([p1,p2,p3])
+promise_all_race.then((value)=>{
+    console.log(value);
+})
+
+let promise_all_any=Promise.any([p1,p2,p3])
+promise_all_any.then((value)=>{
+    console.log(value);
+})
+
+
 
