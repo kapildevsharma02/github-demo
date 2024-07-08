c7aa55ca3010300f654ed6eebfe48b1343f4d310
kapildevsharma02 <kdevsharma012@gmail.com>
2024-07-08 22:26:03 +0530

    Use of allSettled Example in the Promises

--- web_devlopment_practise/PromiseAPIScript.js
+++ web_devlopment_practise/PromiseAPIScript.js
@@ -1,5 +1,7 @@
-// So in this version we have seen there is three promises what if we 
-//want the result at the same time we can do it by using this way 
+//This is the example of the allSettled Promise Example in which suppose if there is a promise which
+// gets failed then with the help of the Promise.all the function will not able to return the promise 
+// therefore it will cause or debrupt abnormaly
+// In order to avoid this we will use Promise.allSettled
 let p1=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         // console.log("Value 1")
@@ -10,7 +12,7 @@ let p1=new Promise((resolve,reject)=>{
 let p2=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         // console.log("Value 2")
-        resolve("Value 2")
+        reject(new Error("This is the error in Promise 2"))
     },2000)
 })
 
@@ -33,7 +35,7 @@ let p3=new Promise((resolve,reject)=>{
 //     console.log(value)
 // })
 
-let promise_all=Promise.all([p1,p2,p3])
+let promise_all=Promise.allSettled([p1,p2,p3])
 promise_all.then((value)=>{
     console.log(value)
 })
