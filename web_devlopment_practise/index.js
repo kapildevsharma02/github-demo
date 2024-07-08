15ca921ff6557d1535b5f8f2c3ad396273b85cea
kapildevsharma02 <kdevsharma012@gmail.com>
2024-07-08 11:14:12 +0530

    These are the recent changes!!

--- web_devlopment_practise/index.js
+++ web_devlopment_practise/index.js
@@ -1,4 +1,22 @@
-console.log("Hi Its Working!!")
-setTimeout(()=>{
-    alert("Hi Its set timeout function is working!")
-},5000)
+console.log("HI")
+let p1=new Promise((resolve,reject)=>{
+    // alert("Hey I am not Resolved")
+    setTimeout(()=>{
+        resolve(1);
+    },2000)
+});
+
+p1.then((value)=>{
+    console.log("Hurray")
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(4);
+        },6000)
+    })
+}).then((value)=>{
+    console.log(value)
+})
+
+p1.then(()=>{
+   console.log("Congratulations this promise is now resolved")
+})
