427b4df0279f3830ac08a855feaff327453920ef
kapildevsharma02 <kdevsharma012@gmail.com>
2024-07-08 18:31:20 +0530

    Promise APIFiles are added

---
+++ web_devlopment_practise/PromiseAPIScript.js
@@ -0,0 +1,35 @@
+// So in this version we have seen there is three promises what if we 
+//want the result at the same time we can do it by using this way 
+let p1=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        // console.log("Value 1")
+        resolve("Value 1")
+    },1000)
+})
+
+let p2=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        // console.log("Value 2")
+        resolve("Value 2")
+    },2000)
+})
+
+let p3=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        // console.log("Value 3")
+        resolve("Value 3")
+    },3000)
+})
+
+p1.then((value)=>{
+    console.log(value)
+})
+
+p2.then((value)=>{
+    console.log(value)
+})
+
+p3.then((value)=>{
+    console.log(value)
+})
+
