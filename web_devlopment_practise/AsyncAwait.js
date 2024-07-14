1cff5684735090d03ffce13fc429446c62f60439
kdevsharma02 <kdevsharma012@gmail.com>
2024-07-14 15:22:08 +0530

    Just for checking why it is showing the unstaged changes in visual studio code

--- web_devlopment_practise/AsyncAwait.js
+++ web_devlopment_practise/AsyncAwait.js
@@ -1,37 +1,44 @@
 //This is the practical example of the asyncawait example
 //by appending the asyc at the beginning of the function it allows us to use then method 
 
-function sum(a,b){
-    return a+b;
-}
+// function sum(a,b){
+//     return a+b;
+// }
 
-async function harry(){
-    let delhiWeather=new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve("27 Deg")
-        },2000)
-    })
+// async function harry(){
+//     let delhiWeather=new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve("27 Deg")
+//         },2000)
+//     })
 
-    let bangaloreWeather=new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve("21 Deg")
-        },5000)
-    })
+//     let bangaloreWeather=new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve("21 Deg")
+//         },5000)
+//     })
 
-    console.log("Fetching Delhi Weather Please Wait ")
-    let delhiW=await delhiWeather
-    console.log("Fetched Delhi Weather "+delhiW)
+//     console.log("Fetching Delhi Weather Please Wait ")
+//     let delhiW=await delhiWeather
+//     console.log("Fetched Delhi Weather "+delhiW)
 
-    let bangaloreW=await bangaloreWeather
-    console.log("Fetched Bangalore Weather "+bangaloreW)
-    return [delhiW,bangaloreW]
-}
-console.log("Welcome to Weather Control Room")
-const cherry=(()=>{console.log("Hey I am cheery and I am not Waiting!")})
-let a=harry()
-let b=cherry()
-console.log(a)
-a.then((value)=>{console.log(value)})
+//     let bangaloreW=await bangaloreWeather
+//     console.log("Fetched Bangalore Weather "+bangaloreW)
+//     return [delhiW,bangaloreW]
+// }
+// console.log("Welcome to Weather Control Room")
+// const cherry=(()=>{console.log("Hey I am cheery and I am not Waiting!")})
+// let a=harry()
+// let b=cherry()
+// console.log(a)
+// a.then((value)=>{console.log(value)})
 
-const s=sum(2,3);
-console.log("The sum is "+s);
+// const s=sum(2,3);
+// console.log("The sum is "+s);
+
+async function fetchData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const json = await response.json();
+    console.log(json);
+  }
+  fetchData();
