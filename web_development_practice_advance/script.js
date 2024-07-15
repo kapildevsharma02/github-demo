e81517d74bcfbfe3158161d75f1326206e5479c5
kdevsharma02 <kdevsharma012@gmail.com>
2024-07-15 19:00:22 +0530

    Adding the foler of the try and catch

---
+++ web_development_practice_advance/script.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+    console.log("Hacking Wifi...Please Wait...")
+},1000)
+
+setTimeout(()=>{
+    console.log("Fetching username and password....Please wait....")
+},2000)
+
+setTimeout(()=>{
+    console.log("Hacking Rahul's Facebook id.... Please wait...")
+},3000)
+
+setTimeout(()=>{
+    console.log("Username and password of Rahul (+919356000001) fetched....Please wait...")
+},4000)
