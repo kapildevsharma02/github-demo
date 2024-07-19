25f76a74146527d1da906bbfd51c7aab2ce0eb1d
kdevsharma02 <kdevsharma012@gmail.com>
2024-07-19 15:36:05 +0530

    Adding the new ReferenceError and attaching the error.name and error.message

--- web_development_practice_advance/custom_errors.js
+++ web_development_practice_advance/custom_errors.js
@@ -1,5 +1,5 @@
 try{
-    harry
+    throw new ReferenceError("Harry is not good")
 }catch(error){
     console.log("This is the name of the error "+error.name)
     console.log("This is the error message "+error.message)
