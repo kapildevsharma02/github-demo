f73e1d9e324b1f264804a89e8a0cda3630b0ef5a
kdevsharma02 <kdevsharma012@gmail.com>
2024-07-21 15:27:15 +0530

    Introduction of the new Reference Error and the error.name , error.message , error.stack

--- web_development_practice_advance/custom_errors.js
+++ web_development_practice_advance/custom_errors.js
@@ -1,6 +1,8 @@
 try{
+    console.log(harry)
     throw new ReferenceError("Harry is not good")
 }catch(error){
-    console.log("This is the name of the error "+error.name)
-    console.log("This is the error message "+error.message)
+    console.log("The error name is "+error.name)
+    console.log("The error message is "+error.message)
+    console.log("The error stack is "+error.stack)
 }
