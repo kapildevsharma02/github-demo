ca7fdbbfe358d4efa00a7d12e87101a22a09cd59
kdevsharma02 <kdevsharma012@gmail.com>
2024-07-21 15:37:17 +0530

    Throwing custom error!!

--- web_development_practice_advance/custom_errors.js
+++ web_development_practice_advance/custom_errors.js
@@ -1,8 +1,12 @@
 try{
-    console.log(harry)
-    throw new ReferenceError("Harry is not good")
+    let age=prompt("Enter your age");
+    age=Number.parseInt(age)
+    if(age>150)
+    throw new ReferenceError(age+" This is probably not true")
 }catch(error){
     console.log("The error name is "+error.name)
     console.log("The error message is "+error.message)
     console.log("The error stack is "+error.stack)
 }
+
+console.log("The script is still running")
