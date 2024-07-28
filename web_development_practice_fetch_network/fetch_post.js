407f33320d450b7179937294e168eacdc49b8bf9
kdevsharma02 <kdevsharma012@gmail.com>
2024-07-28 19:05:18 +0530

    Adding the code using the same work as we did before using aysnc method and some modification of the function

--- web_development_practice_fetch_network/fetch_post.js
+++ web_development_practice_fetch_network/fetch_post.js
@@ -1,15 +1,23 @@
-let options={
-    method:"POST",
-    headers:{
-        "Content-type":"application/json"
-    },
-    body: JSON.stringify({
-        title: 'Harry',
-        body: 'Bhai',
-        userId: 11100,
-      }),
+const createTodo=async()=>{
+    let options={
+        method:"POST",
+        headers:{
+            "Content-type":"application/json"
+        },
+        body: JSON.stringify({
+            title: 'Harry',
+            body: 'Bhai',
+            userId: 11100,
+        }),
+    }
+    let p=await fetch('https://jsonplaceholder.typicode.com/posts',options)
+    let response=await p.json();
+    return response;
 }
 
-fetch('https://jsonplaceholder.typicode.com/posts',options)
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+const mainFun=async ()=>{
+    let todo=await createTodo()
+    console.log(todo)
+}
+
+mainFun();      
